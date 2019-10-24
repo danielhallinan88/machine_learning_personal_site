b9c5986cf89fe6fd0d798213e502b4d6ed46167d
danielhallinan88 <danielhallinan88@gmail.com>
2019-10-24 08:17:36 -0500

    adding templates

--- app.py
+++ app.py
@@ -7,5 +7,13 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/digit-recognizer', methods=["GET", "POST"])
+def mnist():
+    return render_template("mnist.html")
+
+@app.route('/dog-breed', methods=["GET", "POST"])
+def dogBreed():
+    return render_template("dogBreed.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
