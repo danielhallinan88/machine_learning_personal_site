f33d2aadfc191d54f835d4b04d7485ed8ff6cfcf
danielhallinan88 <danielhallinan88@gmail.com>
2020-01-24 23:20:24 -0600

    mnist client

--- static/js/mnist_canvas.js
+++ static/js/mnist_canvas.js
@@ -7,13 +7,13 @@ var canvas, ctx, flag = false,
 
 var x = "black",
     y = 2;
-    
+
 function init() {
     canvas = document.getElementById('can');
     ctx = canvas.getContext("2d");
     w = canvas.width;
     h = canvas.height;
-  
+
     canvas.addEventListener("mousemove", function (e) {
         findxy('move', e)
     }, false);
@@ -27,7 +27,7 @@ function init() {
         findxy('out', e)
     }, false);
 }
-    
+
 function color(obj) {
     switch (obj.id) {
         case "green":
@@ -54,9 +54,9 @@ function color(obj) {
     }
     if (x == "white") y = 14;
     else y = 2;
-    
+
 }
-    
+
 function draw() {
     ctx.beginPath();
     ctx.moveTo(prevX, prevY);
@@ -66,7 +66,7 @@ function draw() {
     ctx.stroke();
     ctx.closePath();
 }
-    
+
 function erase() {
     var m = confirm("Want to clear");
     if (m) {
@@ -74,22 +74,79 @@ function erase() {
         document.getElementById("canvasimg").style.display = "none";
     }
 }
-    
+
+const upload = (file) => {
+
+};
+
 function save() {
-    document.getElementById("canvasimg").style.border = "2px solid";
+    document.getElementById("can").style.border = "2px solid";
     var dataURL = canvas.toDataURL('image/jpeg');
-    document.getElementById("canvasimg").src = dataURL;
-    document.getElementById("canvasimg").style.display = "inline";
+    //console.log(dataURL);
+    //document.getElementById("can").src = dataURL;
+    //document.getElementById("can").style.display = "inline";
+    testImage = new Image(28, 28);
+    testImage.src = canvas.toDataURL('image/jpeg');
+    //document.body.appendChild(testImage);
+
+    //test_digit = new Image();
+    //test_digit.src = '../images/test_image_1.jpg'
+    //var test_digit = document.getElementById('test_digit').src ;
+    //console.log(test_digit);
+
+    //const input = document.getElementById('test_button');
+    //console.log("TEST FILE: " + input.files[0]);
     
+
+    document.querySelector('#test_button').addEventListener('change', event => {
+      handleImageUpload(event)
+    })
+
+    const handleImageUpload = event => {
+      const files = event.target.files
+      const formData = new FormData()
+      formData.append('image', files[0])
+
+      fetch('http://3.19.232.79:8888/mnist', {
+        method: 'POST',
+        body: formData
+      })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+      })
+      .catch(error => {
+        console.error(error)
+      })
+    }
+
+/*
+    const options = {
+      method: 'PUT',
+      body: data,
+      //body: testImage,
+      //headers: {
+      //  'Content-Type' : 'image/jpg'
+      //},
+    };
+    fetch('http://3.19.232.79:8888/mnist', options)
+    //fetch('http://3.19.232.79:8888/test')
+      .then((response) => {
+        return response.json();
+      })
+      .then((myJson) => {
+        console.log(myJson);
+      });
+*/
 }
-    
+
 function findxy(res, e) {
     if (res == 'down') {
         prevX = currX;
         prevY = currY;
         currX = e.clientX - canvas.offsetLeft;
         currY = e.clientY - canvas.offsetTop;
-    
+
         flag = true;
         dot_flag = true;
         if (dot_flag) {
